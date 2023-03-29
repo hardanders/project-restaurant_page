7e59b03f63f4b84e2dda75237cabe8e54aebcafd
hardanders <andyyy85@gmail.com>
2023-03-29 15:46:51 -0400

    Working out basic logic for buttons/changing tabs

---
+++ src/homepage.js
@@ -0,0 +1,18 @@
+function homePage() {
+    const container = document.createElement('div');
+
+    const description = document.createElement('div');
+    description.classList.add('desc');
+    description.innerHTML = 
+    `
+    This Italian Restaurant is a parade for the senses you will not want to miss. From our delicious wines created at our partner vineyard to the
+    savory dishes we will make fresh for you. ` + `<br>` + `<br>` + `Stop in to This Italian Restaurant today and see what you've been missing!
+    `
+    
+    container.classList.add('container');
+    container.append(description);
+    
+    return container;
+};
+
+export default homePage;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
-console.log("Hello, world!");
+import _ from 'lodash';
+import './styles.css';
+import homePage from './homepage';
+
+
+const content = document.getElementById('content')
+
+const header = document.createElement('div');
+header.classList.add('head');
+header.innerHTML = 'Italian Restaurant';
+
+const menu = document.createElement('button');
+const about = document.createElement('button');
+menu.classList.add('menu-btn');
+about.classList.add('about-btn');
+
+const wrapper = document.createElement('div');
+wrapper.classList.add('btn-wrapper');
+wrapper.append(menu, about);
+
+content.appendChild(header);
+content.appendChild(wrapper);
+
+content.appendChild(homePage());
